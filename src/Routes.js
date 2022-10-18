5bfe4f7ff1aed84d9bf4755899f8da2de4a387f5
chintalasaicharan <chintalasaicharan@gmail.com>
2022-10-18 11:31:23 +0530

    food

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import App from './App';
+import Food_library from './components/food_library/Food_library';
 import HelloWorld from './helloWorld';
 
 const Routess = () => {
@@ -10,6 +11,7 @@ const Routess = () => {
         <Routes>
             <Route path='/' element={< App/>}/>
             <Route path='/hello' element={< HelloWorld/>}/>
+            <Route path='/Food_library' element={< Food_library/>}/>
         </Routes>
         </BrowserRouter>
     </div>
---
+++ src/components/food_library/Food_library.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Food_library = () => {
+  return (
+    <div>Food_library</div>
+  )
+}
+
+export default Food_library
