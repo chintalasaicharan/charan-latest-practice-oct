64d87c1c89693683023dcdd22d94a8d8307929ba
chintalasaicharan <chintalasaicharan@gmail.com>
2022-12-21 10:57:03 +0530

    food

--- src/components/food_library/Food_library.js
+++ src/components/food_library/Food_library.js
@@ -1,8 +1,23 @@
-import React from 'react'
+
+import React,{useState} from 'react'
 
 const Food_library = () => {
+  const [search,setSearch]=useState('')
+
+  const submitHandler =e=>{
+    e.preventDefault();
+    console.log(search);
+  }
   return (
-    <div>Food_library</div>
+    <div>
+      <center>
+        <h3>Food_library</h3> <br/>
+        <form onSubmit={submitHandler}>
+          <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}/> <br/> <br/>
+          <input type="submit" value="search" />
+        </form>
+      </center>
+    </div>
   )
 }
 
